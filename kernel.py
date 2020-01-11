4083f5e580410250ecb1f8c7cb40ef93fd7da5fd
Imran Ahmed <Imran_Ahmed@mckinsey.com>
2020-01-11 18:41:42 +0530

    Added full vectorization to kernel class, epsiolon

--- kernel.py
+++ kernel.py
@@ -14,12 +14,14 @@ class Kernel(object):
                        f_vectorized = None,
                        f_vectorized_increment = None,
                        n_dimensions_in = None,
+                       epsilon = 1e-8,
                        **kwargs):
         self.f_pairwise = f_pairwise
         self.f_vectorized = f_vectorized
         self.f_vectorized_increment = f_vectorized_increment
         self.params = kwargs
         self.n_dim_in = n_dimensions_in
+        self.epsilon = epsilon
         self.__validate_kernel()
     
     def __validate_kernel(self):
@@ -43,25 +45,40 @@ class Kernel(object):
                     i_j_cov = self.f_pairwise(X[i, :], X[j, :], self.params) # Pairwise
                     matrix[i, j] = i_j_cov
                     matrix[j, i] = i_j_cov
+            matrix = matrix + torch.eye(X.shape[0])*self.epsilon
             return matrix
         else:
-            return self.f_vectorized(X, self.params)
+            return self.f_vectorized(X, self.params) + torch.eye(X.shape[0])*self.epsilon
 
-    def __increment_matrix(self, X):
-        matrix = torch.empty(X.shape[0], X.shape[0])
-        matrix[:-1, :-1] = self.kernel
+    def __increment_matrix(self, X, X_inc):
+        matrix = torch.empty(X.shape[0] + X_inc.shape[0],
+                             X.shape[0] + X_inc.shape[0])
+        matrix[:X.shape[0], 
+               :X.shape[0]] = self.kernel
         increment_kernel = None
+        X_concat = None 
         if self.f_vectorized_increment is None:
-            for i in range(X.shape[0]):
-                i_j_cov = self.f_pairwise(X[i, :], X[-1, :], self.params) 
-                matrix[-1, i] = i_j_cov
-                matrix[i, -1] = i_j_cov
-            increment_kernel = matrix[-1, :]
+            X_concat = torch.cat([X, X_inc], 0)
+            for c_idx in range(X_inc.shape[0]):
+                fill_idx = X.shape[0] + c_idx
+                for i in range(X_concat.shape[0]):
+                    i_j_cov = self.f_pairwise(X_concat[i, :], 
+                                              X_concat[fill_idx, :], 
+                                              self.params) 
+                    matrix[fill_idx, i] = i_j_cov
+                    matrix[i, fill_idx] = i_j_cov
+                matrix[fill_idx, fill_idx] += self.epsilon
+            increment_kernel = matrix[X.shape[0]:, :]
         else:
-            increment_kernel = self.f_vectorized_increment(X, self.params)
-            matrix[-1, :] = increment_kernel
-            matrix[:, -1] = increment_kernel
-        return matrix, increment_kernel
+            increment_kernel = self.f_vectorized_increment(X, 
+                                                           X_inc, 
+                                                           self.params)
+            epsilon_mat = torch.eye(X_inc.shape[0])*self.epsilon
+            increment_kernel[X.shape[0]:, :] += epsilon_mat
+            matrix[X.shape[0]:, :] = increment_kernel.permute(1, 0)
+            matrix[:, X.shape[0]:] = increment_kernel
+            X_concat = torch.cat([X, X_inc], 0)
+        return matrix, increment_kernel, X_concat
 
     def cast(self, X): 
         # Cast from numpy array
@@ -92,19 +109,18 @@ class Kernel(object):
         if self.kernel is None: 
             raise ValueError("Cannot run predict_increment before running fit on training data")
         X_inc = self.cast(X_inc)
-        
-        if len(X_inc.shape) != 1: 
-            raise ValueError('Expected vector input but received tensor of order {} and shape {}'.format(len(X_inc.shape), X_inc.shape))
+        if len(X_inc.shape) == 1:
+            X_inc = X_inc.unsqueeze(0)
+        if len(X_inc.shape) != 2: 
+            raise ValueError('Expected matrix input but received tensor of order {} and shape {}'.format(len(X_inc.shape), X_inc.shape))
         if (X_inc.shape[-1] != self.n_dim_in):
             raise ValueError('Expected vector of dimension {}, got vector of dimension {}'.format(self.n_dim_in, X_inc.shape[-1]))
-      
-        X_new = torch.cat([self.X, X_inc.unsqueeze(0)], 0)
-        return self.__increment_matrix(X_new)
+        return self.__increment_matrix(self.X, X_inc)
 
     def fit_increment(self, X_inc):
-        kernel_new, kernel_increment = self.predict_increment(X_inc)
-        self.X = torch.cat([self.X, X_inc.unsqueeze(0)], 0)
-        self.kernel = kernel_new
+        matrix_new, _ , X_new = self.predict_increment(X_inc)
+        self.X = X_new
+        self.kernel = matrix_new
         return self
 
 class SquaredExponential(Kernel):
@@ -132,8 +148,24 @@ class SquaredExponential(Kernel):
             kernel = torch.sum(kernel, 0)
             return torch.exp(-1/(2*params['tau']**2) * kernel)
 
-        def f_vectorized_increment(X, params):
-            return torch.exp(-1/(2*params['tau']**2) *  torch.pow(torch.norm(X - X[-1, :], dim = 1, p = "fro"), 2))
+        def f_vectorized_increment(X, X_inc, params):
+            inc_batch_size = X_inc.shape[0]
+            base_batch_size = X.shape[0]
+            X_concat = torch.cat([X, X_inc], 0)
+            X_concat = X_concat.permute(1, 0)
+            X_inc = X_inc.permute(1, 0)
+
+            X_left_square = torch.unsqueeze(X_concat, -1)
+            X_right_square = torch.transpose(torch.unsqueeze(X_inc, -1), -2, -1)
+            X_left_square_pow = torch.pow(torch.repeat_interleave(X_left_square,
+                                                                  inc_batch_size, 
+                                                                  dim = -1), 2)
+            X_right_square_pow = torch.pow(torch.repeat_interleave(X_right_square,
+                                                                  base_batch_size + inc_batch_size, 
+                                                                  dim = -2), 2)
+            X_centre = torch.bmm(X_left_square, X_right_square)
+            kernel = torch.sum(X_left_square_pow + X_right_square_pow - 2*X_centre, 0)
+            return torch.exp(-1/(2*params['tau']**2) * kernel)
 
         if use_pairwise_only:
             super().__init__(f_pairwise = f_pairwise, 
@@ -180,9 +212,25 @@ class ARDSquaredExponential(Kernel):
             kernel = torch.matmul(kernel.permute(1, 2, 0), 1/params['lengthscale']) # Dot and reduce
             return params['sigma']**2 * torch.exp(-1/2 * kernel)
 
-        def f_vectorized_increment(X, params):
-            inc_vec = torch.matmul(torch.pow(X-X[-1, :], 2), 1/params['lengthscale'])
-            return params['sigma']**2 * torch.exp(-1/2 * inc_vec)
+        def f_vectorized_increment(X, X_inc, params):
+            inc_batch_size = X_inc.shape[0]
+            base_batch_size = X.shape[0]
+            X_concat = torch.cat([X, X_inc], 0)
+            X_concat = X_concat.permute(1, 0)
+            X_inc = X_inc.permute(1, 0)
+
+            X_left_square = torch.unsqueeze(X_concat, -1)
+            X_right_square = torch.transpose(torch.unsqueeze(X_inc, -1), -2, -1)
+            X_left_square_pow = torch.pow(torch.repeat_interleave(X_left_square,
+                                                                  inc_batch_size, 
+                                                                  dim = -1), 2)
+            X_right_square_pow = torch.pow(torch.repeat_interleave(X_right_square,
+                                                                  base_batch_size + inc_batch_size, 
+                                                                  dim = -2), 2)
+            X_centre = torch.bmm(X_left_square, X_right_square)
+            kernel = X_left_square_pow + X_right_square_pow - 2*X_centre
+            kernel = torch.matmul(kernel.permute(1, 2, 0), 1/params['lengthscale']) # Dot and reduce
+            return params['sigma']**2 * torch.exp(-1/2 * kernel)
 
         if use_pairwise_only:
             super().__init__(f_pairwise = f_pairwise, 
@@ -197,23 +245,30 @@ class ARDSquaredExponential(Kernel):
                             **{'sigma': sigma, 'lengthscale':lengthscale})         
 
 if __name__ == '__main__':
-    test_feat = torch.empty(15, 4).uniform_(0, 1)
-    add_feat = torch.empty(4).uniform_(0, 1)
+    test_feat = torch.empty(3, 4).uniform_(0, 1)
+    add_feat = torch.empty(3, 4).uniform_(0, 1)
+    full_feat = torch.cat([test_feat, add_feat], 0)
 
+    print('---------------------------------------------')
     print('Comparison of vectorised and pairwise kernel outputs (SE kernel)')
     SE_pw_test = SquaredExponential(tau = 1, use_pairwise_only = True).fit(test_feat)
     SE_vec_test = SquaredExponential(tau = 1, use_pairwise_only = False).fit(test_feat)
+    SE_full_test = SquaredExponential(tau = 1, use_pairwise_only = False).fit(full_feat)
     assert torch.allclose(SE_vec_test.fit(test_feat).kernel, SE_pw_test.fit(test_feat).kernel)
-    assert torch.allclose(SE_vec_test.fit_increment(add_feat).kernel, SE_pw_test.fit_increment(add_feat).kernel)
+    assert torch.allclose(SE_pw_test.predict_increment(add_feat)[0], SE_full_test.kernel)
+    assert torch.allclose(SE_vec_test.predict_increment(add_feat)[0], SE_full_test.kernel)
+    
     print('---------------------------------------------')
-
     print('Comparison of vectorised and pairwise kernel outputs (ARD SE kernel)')
+    test_feat = torch.empty(15, 5).uniform_(0, 1)
+    add_feat = torch.empty(3, 5).uniform_(0, 1)
+    full_feat = torch.cat([test_feat, add_feat], 0)
     ARD_SE_pw_test = ARDSquaredExponential(sigma = 1, lengthscale= [1, 2, 3, 4, 5], n_dimensions_in = 5, use_pairwise_only= True)
     ARD_SE_vec_test = ARDSquaredExponential(sigma = 1, lengthscale= [1, 2, 3, 4, 5], n_dimensions_in = 5, use_pairwise_only= False)
-    test_feat = torch.empty(15, 5).uniform_(0, 1)
-    add_feat = torch.empty(5).uniform_(0, 1)
+    ARD_full_test =  ARDSquaredExponential(sigma = 1, lengthscale= [1, 2, 3, 4, 5], n_dimensions_in = 5, use_pairwise_only= False).fit(full_feat)
     assert torch.allclose(ARD_SE_pw_test.fit(test_feat).kernel, ARD_SE_vec_test.fit(test_feat).kernel)
-    assert torch.allclose(ARD_SE_pw_test.fit_increment(add_feat).kernel, ARD_SE_vec_test.fit_increment(add_feat).kernel)
+    assert torch.allclose(ARD_SE_pw_test.predict_increment(add_feat)[0], ARD_full_test.kernel)
+    assert torch.allclose(ARD_SE_vec_test.predict_increment(add_feat)[0], ARD_full_test.kernel)
 
     print('---------------------------------------------')
     print('Timing test (fit method)...')
