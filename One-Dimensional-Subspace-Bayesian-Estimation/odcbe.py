3ff1cdcb0501a97f85048fc558769a13f2fadffd
moretanks <c-chen@hotmail.co.uk>
2020-04-26 14:49:55 +0100

    Use SciPy Brent optimiser for the linear opt

--- One-Dimensional-Subspace-Bayesian-Estimation/odcbe.py
+++ One-Dimensional-Subspace-Bayesian-Estimation/odcbe.py
@@ -1,5 +1,6 @@
 from test_gaussian_proc import dumb_gaussian as gaussian, kernels
 import numpy as np
+from scipy.optimize import minimize_scalar
 
 
 class bound():
@@ -11,7 +12,7 @@ class bound():
         for boundary in self.boundaries:
             assert boundary[0] < boundary[1]
 
-    def in_boundary(self, x):
+    def _in_boundary(self, x):
         # given x, checks if the vector is inside the space
         assert len(x) == len(self.boundaries)
         for i, boundary in enumerate(self.boundaries):
@@ -19,7 +20,7 @@ class bound():
                 return False
         return True
 
-    def line_plane_intersection(self, x, c, n, k):
+    def _line_plane_intersection(self, x, c, n, k):
         # Given line x+ct and plane n.v = k, find the point of intersection
         # return None if it doesn't exist.
         perp = np.dot(n, c)  # this is the component of c perpendicular to the plane
@@ -31,7 +32,8 @@ class bound():
     def find_endpoints(self, x, c):
         # Given line x+ct, find the two points where they intersect
         # Assume that c is a unit vector
-        # Since we're dealing with random vectors, we don't need to care about edge cases
+        # Since we're dealing with random vectors,
+        # we don't need to care about edge cases
         # as much -- just retry in those cases
         assert len(x) == len(self.boundaries)
         assert len(c) == len(self.boundaries)
@@ -40,8 +42,8 @@ class bound():
             n = np.zeros(len(self.boundaries))
             n[i] = 1.0
             for k in bound:
-                intersect = self.line_plane_intersection(x, c, n, k)
-                if self.in_boundary(intersect):
+                intersect = self._line_plane_intersection(x, c, n, k)
+                if self._in_boundary(intersect):
                     endpoints.append(intersect)
         if len(endpoints) != 2:
             raise FloatingPointError(
@@ -75,25 +77,16 @@ class ODSBE():
             except FloatingPointError:
                 continue
         if not endpoints:
-            raise FloatingPointError()  # TODO: change this to the correct error type, create error hierarchy later
+            raise ValueError("No Endpoints found")
 
         def constrained_opt(t):
             # constrained linear problem. Solve for t in [0,1]
             input = (1 - t) * endpoints[0] + t * endpoints[1]
-            return self.f_gaussian.evaluate(input)
+            return np.ndarray.item(self.f_gaussian.evaluate(input))
 
-        label, arg = self._dumb_1d_opt(constrained_opt, 100)
-        return (1 - arg) * endpoints[0] + arg * endpoints[1]
+        scalar_result = minimize_scalar(constrained_opt, bounds=(0, 1), method='bounded')
+        return (1 - scalar_result.x) * endpoints[0] + scalar_result.x * endpoints[1]
 
-    def _dumb_1d_opt(self, f, h):
-        # TODO: This is incredibly crude. replace it ASAP
-        arg, label = 0, f(0)
-        for x in np.linspace(0, 1, h):
-            new_label = f(x)
-            if new_label > label:
-                label = new_label
-                arg = x
-        return label, arg
 
     def _add_data(self, label, input):
         self.f_gaussian.add_single_data(label, input)
@@ -107,18 +100,16 @@ class ODSBE():
         for _ in range(iter):
             new_input = self._random_vector_opt(self.data['inputs'][-1])
             self._add_data(self.f(new_input), new_input)
-        return self.data['labels'][-1],self.data['inputs'][-1]
+        return self.data['labels'][-1], self.data['inputs'][-1]
+
 
 if __name__ == '__main__':
-    b = bound([(-1, 1), (-1, 1)])
-    func = lambda x: -(np.linalg.norm(x)**2)
-    optimser = ODSBE(
+    b = bound([(-1, 1), (-1, 1), (-1, 1)])
+    func = lambda x: (np.linalg.norm(x + np.array([0.1, 0.1, 0])) ** 2)
+    optimiser = ODSBE(
         kernels.SQUARE_EXP,
         func,
         b,
-        length = 1
+        length=1.5
     )
-    print(optimser.opt(10))
-
-
-
+    print(optimiser.opt(100))
